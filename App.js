07ba3b8f85251b7f8d1cdf70d1abcd131a84d58c
Dan Constantinescu <dan_constantinescu@outlook.com>
2018-01-25 17:20:53 +0200

    defined the store

--- App.js
+++ App.js
@@ -5,34 +5,22 @@
  */
 
 import React, { Component } from 'react';
-import {
-  Platform,
-  StyleSheet,
-  Text,
-  View
-} from 'react-native';
+import { View, StyleSheet } from 'react-native';
+import { Provider } from 'react-redux';
+import { Header, CryptoContainer } from './src/components';
 
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' +
-    'Cmd+D or shake for dev menu',
-  android: 'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
 
-export default class App extends Component<{}> {
+import Store from './src/Store';
+
+export default class App extends Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit App.js
-        </Text>
-        <Text style={styles.instructions}>
-          {instructions}
-        </Text>
-      </View>
+      <Provider store={Store}>
+        <View style={styles.container}>
+          <Header />
+          <CryptoContainer />
+        </View>
+      </Provider>
     );
   }
 }
---
+++ src/Reducers/CryptoReducer.js
@@ -0,0 +1,9 @@
+// export default function(state = [], action) {
+//     return state;
+// };
+
+const CryptoReducer = (state = [], action) => {
+    return state;
+};
+
+export default CryptoReducer;
