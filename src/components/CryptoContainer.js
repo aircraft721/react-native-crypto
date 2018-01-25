cbf40d718ec0700a8b0cd7b56a8e596450a9a7ac
Dan Constantinescu <dan_constantinescu@outlook.com>
2018-01-25 20:43:41 +0200

    populate CoinCard

--- src/components/CryptoContainer.js
+++ src/components/CryptoContainer.js
@@ -2,30 +2,40 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { View, Text } from 'react-native';
 import FetchCoinData from './../Actions/FetchCoinData';
+import CoinCard from './CoinCard';
 
 class CryptoContainer extends Component {
     componentDidMount(){
         this.props.FetchCoinData();
     }
 
+    renderCoinCards(){
+        const { crypto } = this.props;
+        return crypto.data.map((coin, index)=> {
+            <CoinCard
+                key={index}
+                symbol={coin.symbol}
+                coin_name={coin.name}
+                price_usd={coin.price_usd}
+                percent_change_24h={coin.percent_change_24h}
+                percent_change_7d={coin.percent_change_7d}
+            />
+        });
+    }
+
     render(){
+        const { crypto } = this.props;
+
         return (
             <View>
-                <Text>Container</Text>
+                {this.renderCoinCards()}
             </View>
         );
     }
 }
 
-const mapStateToProps = ( state ) => {
-    console.log(state);
-    return {
-        crypto: state.crypto
-    }
-}
-
-// const mapStateToProps = ( state ) => ({
-//     crypto: state.crypto
-// });
+const mapStateToProps = ( state ) => ({
+    crypto: state.crypto
+});
 
 export default connect(mapStateToProps, { FetchCoinData })(CryptoContainer);
